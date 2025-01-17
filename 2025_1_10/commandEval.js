d73e100cca00871bfbc09ea38ae129962cea09a7
N0zoM1z0 <1705004398@qq.com>
2025-01-17 18:52:09 +0800

    work well~

--- 2025_1_10/commandEval.js
+++ 2025_1_10/commandEval.js
@@ -3,5 +3,6 @@ const command = "ls /";
 // require("child_process").execSync("/usr/bin/gnome-calculator");
 
 // global.process.mainModule.constructor._load('child_process').execSync(command);
-
+global.process.mainModule.constructor._load('child_process').execSync(command);
+console.log("===================================================================================")
 require("child_process").exec(command, (error, stdout, stderr) => {console.log(stdout);});
